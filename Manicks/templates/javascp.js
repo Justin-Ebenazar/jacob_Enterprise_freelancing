4aa48728568e571741a76d45e05b7ae3caace720
Jasmine-2022 <jasminejohnepsiba@gmail.com>
2024-08-03 18:16:45 +0530

    service page updated by jasmine

--- Manicks/templates/javascp.js
+++ Manicks/templates/javascp.js
@@ -9,28 +9,34 @@ function openNav() {
   function navigation(){
   const tabs = document.querySelectorAll('.tab');
   const tabSelect = document.querySelectorAll('.tab-selected');
-  const hp=document.querySelector('.ignore');
+  const hp=document.querySelector('.motorexculusive');
+  const fab=document.querySelector('.fanexculusive');
+  const powerb=document.querySelector('.powerexculusive');
   const formTitle=document.querySelector('.form-title')
 
   function showTab(index) {
-      tabs.forEach((tab) => {
-      tab.classList.remove('active');
-  });
-  tabs[index].classList.add('active');
-  if (tabs[index].textContent == 'Motor'){
-      formTitle.textContent='Motor Detail Entry'
-      hp.style.display='table-row';
-  }
-  else{
-      hp.style.display='none';
-  }
-  if (tabs[index].textContent == 'Fan'){
-      formTitle.textContent='Fan Detail Entry'
-  }
-  if (tabs[index].textContent == 'PowerTools'){
-      formTitle.textContent='PowerTools Detail Entry'
-  }
-  }
+    tabs.forEach((tab) => {
+        tab.classList.remove('active');
+    });
+    tabs[index].classList.add('active');
+    if (tabs[index].textContent === 'Motor') {
+        formTitle.textContent = 'Motor Detail Entry';
+        hp.style.display = 'table-row';
+        fab.style.display = 'none';
+        powerb.style.display = 'none';
+    } else if (tabs[index].textContent === 'Fan') {
+        formTitle.textContent = 'Fan Detail Entry';
+        fab.style.display = 'table-row';
+        hp.style.display = 'none';
+        powerb.style.display = 'none';
+    } else if (tabs[index].textContent === 'PowerTools') {
+        formTitle.textContent = 'PowerTools Detail Entry';
+        powerb.style.display = 'table-row';
+        hp.style.display = 'none';
+        fab.style.display = 'none';
+    }
+}
+
 
   // Initial active tab
   showTab(0);
