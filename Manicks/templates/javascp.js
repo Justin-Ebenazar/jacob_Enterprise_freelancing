21865fe5398ab641dcbffa336fdaeb6b6bf43c44
Manikarajhp <manikaraj480@gmail.com>
2024-08-05 16:59:27 +0530

    chanded

--- Manicks/templates/javascp.js
+++ Manicks/templates/javascp.js
@@ -9,40 +9,46 @@ function openNav() {
   function navigation(){
   const tabs = document.querySelectorAll('.tab');
   const tabSelect = document.querySelectorAll('.tab-selected');
-  const hp=document.querySelector('.motorexclusive');
-  const fab=document.querySelector('.fanexclusive');
-  const powerb=document.querySelector('.powerexclusive');
+  const motor_ignore=document.querySelector('.motor-ignore');
+  const fan_ignore=document.querySelector('.fan-ignore');
+  const powertool_ignore=document.querySelectorAll('.powertool-ignore');
+  
   const formTitle=document.querySelector('.form-title')
 
   function showTab(index) {
-    tabs.forEach((tab) => {
-        tab.classList.remove('active');
-    });
-    tabs[index].classList.add('active');
-    console.log("God is Good");
-    
-    hp.style.display
-    if (tabs[index].textContent == 'Fan') {
-        /*formTitle.textContent = 'Fan Detail Entry';*/
-        hp.style.display='table-row';
-        fab.style.display='none';
-        powerb.style.display='none';
-    } 
-    else if (tabs[index].textContent == 'Fan') { 
-        /*formTitle.textContent = 'Fan Detail Entry';*/
-        fab.style.display = 'table-row';
-        hp.style.display = 'none';
-        powerb.style.display = 'none';
-    } else{
-        /*formTitle.textContent = 'PowerTools Detail Entry';*/
-        powerb.style.display = 'table-row';
-        hp.style.display = 'none';
-        fab.style.display = 'none';
+      tabs.forEach((tab) => {
+      tab.classList.remove('active');
+  });
+  tabs[index].classList.add('active');
+  if (tabs[index].textContent == 'Motor'){
+      formTitle.textContent='Motor Detail Entry'
+      motor_ignore.style.display='table-row';
+  }
+  else{
+      motor_ignore.style.display='none';
+  }
+  if (tabs[index].textContent == 'Fan'){
+      formTitle.textContent='Fan Detail Entry';
+      fan_ignore.style.display='table-row';
     }
-}
+    else{
+        fan_ignore.style.display='none';
+    }
+  if (tabs[index].textContent == 'PowerTools'){
+      formTitle.textContent='PowerTools Detail Entry'
+      powertool_ignore.forEach((tool)=>{
+        tool.style.display='table-row';
+    });
+    }
+    else{
+        powertool_ignore.forEach((tool)=>{
+            tool.style.display='none';
+        });
+    }
+  }
 
   // Initial active tab
-  /*showTab(0);*/
+  showTab(0);
 
   tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => {
@@ -50,3 +56,4 @@ function openNav() {
   });
   });
 }
+navigation();
