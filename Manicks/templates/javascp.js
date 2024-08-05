54522063f460d983eba976862f3a1459ccf74d42
Justin <thinkgoodone03@gmail.com>
2024-08-05 10:48:35 +0530

    chinna change

--- Manicks/templates/javascp.js
+++ Manicks/templates/javascp.js
@@ -9,9 +9,9 @@ function openNav() {
   function navigation(){
   const tabs = document.querySelectorAll('.tab');
   const tabSelect = document.querySelectorAll('.tab-selected');
-  const hp=document.querySelector('.motorexculusive');
-  const fab=document.querySelector('.fanexculusive');
-  const powerb=document.querySelector('.powerexculusive');
+  const hp=document.querySelector('.motorexclusive');
+  const fab=document.querySelector('.fanexclusive');
+  const powerb=document.querySelector('.powerexclusive');
   const formTitle=document.querySelector('.form-title')
 
   function showTab(index) {
@@ -19,27 +19,30 @@ function openNav() {
         tab.classList.remove('active');
     });
     tabs[index].classList.add('active');
-    if (tabs[index].textContent === 'Motor') {
-        formTitle.textContent = 'Motor Detail Entry';
-        hp.style.display = 'table-row';
-        fab.style.display = 'none';
-        powerb.style.display = 'none';
-    } else if (tabs[index].textContent === 'Fan') {
-        formTitle.textContent = 'Fan Detail Entry';
+    console.log("God is Good");
+    
+    hp.style.display
+    if (tabs[index].textContent == 'Fan') {
+        /*formTitle.textContent = 'Fan Detail Entry';*/
+        hp.style.display='table-row';
+        fab.style.display='none';
+        powerb.style.display='none';
+    } 
+    else if (tabs[index].textContent == 'Fan') { 
+        /*formTitle.textContent = 'Fan Detail Entry';*/
         fab.style.display = 'table-row';
         hp.style.display = 'none';
         powerb.style.display = 'none';
-    } else if (tabs[index].textContent === 'PowerTools') {
-        formTitle.textContent = 'PowerTools Detail Entry';
+    } else{
+        /*formTitle.textContent = 'PowerTools Detail Entry';*/
         powerb.style.display = 'table-row';
         hp.style.display = 'none';
         fab.style.display = 'none';
     }
 }
 
-
   // Initial active tab
-  showTab(0);
+  /*showTab(0);*/
 
   tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => {
