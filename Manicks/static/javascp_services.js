680cd3e2d6ee185c67fe8de9f7a9c264ded7c578
Justin <thinkgoodone03@gmail.com>
2024-09-05 18:15:00 +0530

    animations

--- Manicks/static/javascp_services.js
+++ Manicks/static/javascp_services.js
@@ -1,5 +1,3 @@
-  //services page tab switch code 
-
   // code for sidepane
 
   function openNav() {
@@ -13,7 +11,10 @@
 
 // end of side pane code
 
-// JavaScript Code
+
+
+// tab change start here
+/*
 const tabs = document.querySelectorAll(".tab");
 const tabsContant = document.querySelectorAll(".tab-contant");
 
@@ -23,10 +24,12 @@ function showTab(index) {
   });
   tabs[index].classList.add("active");
 
+  // Add animation using CSS transitions
   tabsContant.forEach((contant) => {
-    contant.classList.remove("active");
+    contant.style.transition = "width 0.3s ease-in-out"; // Transition animation
+    contant.style.maxWidth = 300; // Initially hide all content
   });
-  tabsContant[index].classList.add("active");
+  tabsContant[index].style.maxWidth = tabsContant[index].scrollWidth + "px"; // Expand selected content
 }
 
 showTab(0); // Show the first tab initially
@@ -35,7 +38,42 @@ tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
     showTab(index);
   });
-});
+});*/
+
+// tabc chnage end
+function showTab(a){
+  
+  if(a==0){
+    document.getElementById("fan").style.width="100%";
+    document.getElementById("motor").style.width="0px";
+    document.getElementById("power").style.width="0px";
+    
+    /*document.getElementById("fan").style.opacity="1";
+    document.getElementById("motor").style.opacity="0";
+    document.getElementById("power").style.opacity="0";*/
+  }
+  else if(a==1){
+    document.getElementById("fan").style.width="0";
+    document.getElementById("motor").style.width="100%";
+    document.getElementById("power").style.width="0px";
+
+    /*document.getElementById("fan").style.opacity="0";
+    document.getElementById("motor").style.opacity="1";
+    document.getElementById("power").style.opacity="0";*/
+  }
+  else{
+    document.getElementById("fan").style.width="0px";
+    document.getElementById("motor").style.width="0px";
+    document.getElementById("power").style.width="100%";
+
+    /*document.getElementById("fan").style.opacity="0";
+    document.getElementById("motor").style.opacity="0";
+    document.getElementById("power").style.opacity="1";*/
+  }
+
+}
+
+
 function additemActive(){
   const additem=document.querySelector('.additem');
   additem.style.display="flex";
